bb96a91f61f4c92ab6720bb4ed954979d7a73e01
octaviodegodoy <octaviodegodoy@gmail.com>
2018-01-01 17:02:58 -0200

    Added styling to the About and Examples page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,24 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h2>Welcome to the about page !!</h2>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This is a weather application build on React.
+        I have built for the Complete React Web App Developer Course
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+<a href="https://facebook.github.io/react">React</a>- This was the JavaScript framework used.
+        </li>
+        <li>
+<a href="http://openweathermap.org">Open Weather Map</a>- I used open weather map to search weather by city name.
+        </li>
+      </ul>
+    </div>
+
   )
 };
 
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-     <h2>Examples!</h2>
-     <p>Welcome to examples page !!</p>
+     <h2 className="text-center">Examples!</h2>
+     <p>Here are some examples of locations to try out :</p>
+     <ol>
+<li>
+<Link to='/?location=Botucatu'>Botucatu,Brazil</Link>
+</li>
+<li>
+<Link to='/?location=Cork'>Cork,Ireland</Link>
+</li>
+     </ol>
    </div>
   )
 };
