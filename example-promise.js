a57365628dd510eabaadcea336a7d56cae7aba38
Luiz Godoy <degodoy@localhost.localdomain>
2018-05-28 18:42:57 -0300

    Exemplo promise resolvido para axios

--- example-promise.js
+++ example-promise.js
@@ -43,7 +43,28 @@
 // });
 //
 
+var axios = require('axios');
 
-arr = [1, 2, 3];
-squares = arr.map(x => x * x );
-console.log(squares)
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=65b6172a1308505f06e5dd64c6d631d2&units=metric';
+
+function getTemperature(location){
+
+  var encodedLocation = encodeURIComponent(location);
+  var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+  return axios.get(requestUrl).then(function(response) {
+    if(response.data.cod && response.data.message){
+      throw new Error('Unable to fetch weather ');
+    } else {
+      return response.data.main.temp;
+    }
+  }, function(error){
+    throw new Error(error.response.data.message);
+  });
+}
+const loc = 'Santiago';
+getTemperature(loc).then(function(temp){
+  console.log('Temperatura em',loc,temp,'graus');},
+  function(error){
+    console.log(error);
+  });
